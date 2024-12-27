616f29092d14ebbee9cccb9dfd4011c0ac27d271
Rico Groth <groth.rico@protonmail.com>
2024-12-27 14:53:49 +0100

    add nightfox color scheme with some colorblind values (5 min online test, not that accurate i guess)

--- lua/plugins/color-scheme.lua
+++ lua/plugins/color-scheme.lua
@@ -4,8 +4,31 @@ local everforest = {
     lazy = false,
     priority = 1000,
     config = function()
-	require("everforest").setup({})
+	local everforest = require("everforest")
+	everforest.setup({
+	    background = "hard"
+	})
+	everforest.load()
     end,
+}
+
+local nightfox = {
+    "EdenEast/nightfox.nvim",
+    config = function()
+	require("nightfox").setup({
+	    options = {
+		colorblind = {
+		    enable = true,
+		    severity = {
+			protan = 0.6, -- red
+			deutan = 1.0, -- green
+			tritan = 0.3, -- blue
+		    }
+		},
+	    },
+	})
+	vim.cmd.colorscheme("nightfox")
+    end
 }
 
 local catppuccin = {
@@ -14,12 +37,13 @@ local catppuccin = {
     priority = 1000,
     config = function()
 	require("catppuccin").setup({
-	    flavour = "macchiato", -- latte (light), frappe, macchiato, mocha (darkest)
+	    flavour = "mocha",
 	    integrations = {
 		treesitter = true,
 	    }
 	})
+	vim.cmd.colorscheme("catppuccin")
     end
 }
 
-return catppuccin
+return nightfox
