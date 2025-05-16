619aa5dc4eb08347b58a83c2248de9f1adeb38da
Rico Groth <rico.groth@check24.de>
2025-05-16 18:01:26 +0200

    add stylelint as formatter, increase timeout for formatters to allow stylelint to act

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -9,14 +9,14 @@ local plugin = {
 		conform.setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				javascript = { "prettierd" },
-				javascriptreact = { "prettierd" },
-				typescript = { "prettierd" },
-				typescriptreact = { "prettierd" },
+				javascript = { "prettierd", "stylelint" },
+				javascriptreact = { "prettierd", "stylelint" },
+				typescript = { "prettierd", "stylelint" },
+				typescriptreact = { "prettierd", "stylelint" },
 				php = { "php-cs-fixer" },
 			},
 			format_on_save = {
-				timeout_ms = 500,
+				timeout_ms = 10000,
 				lsp_format = "fallback",
 			},
 		})
