19664922235b8d0eaa0467a0315022c41680f0b3
Rico Groth <rico.groth@check24.de>
2025-05-20 10:05:57 +0200

    use kickstart nvim config to highlight references in current buffer

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -85,5 +85,36 @@ return {
 		vim.keymap.set("n", "<leader>lS", telescope.lsp_workspace_symbols, { noremap = true })
 		vim.keymap.set("n", "<leader>ls", telescope.lsp_document_symbols, { noremap = true })
 		configure_lsps()
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("Custom-lsp-attach", { clear = true }),
+			callback = function(event)
+				local client = vim.lsp.get_client_by_id(event.data.client_id)
+				if
+					client
+					and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf)
+				then
+					local highlight_augroup = vim.api.nvim_create_augroup("Custom-lsp-highlight", { clear = false })
+					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.document_highlight,
+					})
+
+					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.clear_references,
+					})
+
+					vim.api.nvim_create_autocmd("LspDetach", {
+						group = vim.api.nvim_create_augroup("Custom-lsp-detach", { clear = true }),
+						callback = function(event2)
+							vim.lsp.buf.clear_references()
+							vim.api.nvim_clear_autocmds({ group = "Custom-lsp-highlight", buffer = event2.buf })
+						end,
+					})
+				end
+			end,
+		})
 	end,
 }
